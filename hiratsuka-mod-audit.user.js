166e0a29a89dd74ad019428b85648e7ea9967809
yoosuqe69-ai <yoosuqe69@gmail.com>
2026-06-30 16:34:50 +0900

    Create hiratsuka-mod-audit.user.js

---
+++ hiratsuka-mod-audit.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @id             iitc-plugin-hiratsuka-mod-audit
+// @name           IITC plugin: Hiratsuka MOD Audit
+// @category       Info
+// @version        0.0.1
+// @description    test
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
+
+alert('Hiratsuka MOD Audit test loaded');
